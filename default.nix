41ed237025ce872b351cb310529eabf1a727fd11
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-02-22 16:12:38 +0100

    new env-haskell

--- default.nix
+++ default.nix
@@ -1,2 +1,26 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, aeson, base, bimaps, binary, bytestring, cereal
+, cereal-text, compact, containers, criterion, deepseq, hashable
+, lens, QuickCheck, stdenv, string-conversions, tasty
+, tasty-quickcheck, tasty-th, text, text-binary, utf8-string
+, vector-th-unbox
+}:
+mkDerivation {
+  pname = "InternedData";
+  version = "0.0.0.1";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson base bimaps binary bytestring cereal cereal-text compact
+    deepseq hashable lens string-conversions text text-binary
+    utf8-string vector-th-unbox
+  ];
+  testHaskellDepends = [
+    aeson base binary cereal lens QuickCheck string-conversions tasty
+    tasty-quickcheck tasty-th text
+  ];
+  benchmarkHaskellDepends = [
+    base bytestring containers criterion deepseq text
+  ];
+  homepage = "https://github.com/choener/InternedData";
+  description = "Data interning (with compact regions where possible)";
+  license = stdenv.lib.licenses.bsd3;
+}
